ac859e21264bc0dff706de8c375c4b4ce350c8c0
陳昱誠 <46038083+ga877439@users.noreply.github.com>
2019-03-12 22:00:15 +0800

    Update crawler.py
    加了點東西
    方便以後處利

--- ETF_NAV/crawler.py
+++ ETF_NAV/crawler.py
@@ -2,6 +2,8 @@ import login_website
 import json
 import time
 import pandas as pd
+from bs4 import BeautifulSoup
+
 filename = "ETF_List_Filtered.csv"
 file = open(filename, "r")
 #name = "output.csv"
@@ -30,8 +32,19 @@ for i in range(34):
         rj = json.loads(reqs.text)
         to_deal = rj["data_table_html"]
         time.sleep(1)
-        df_list.append(pd.read_html(to_deal, header = 0)[0])
-        #print(type(to_deal))
-    
+        soup = BeautifulSoup(to_deal, 'lxml')    
+
+        col1 = soup.find_all('td',attrs={"class": "col1"})  
+        col2 = soup.find_all('td',attrs={"class": "col2"})  
+        print(col1[0].text.split())	#格式是這樣 ['Dec.', '31,', '2018']	一個list
+		print(col2[0].text.strip())	#格式是這樣 '123.54' 一個字串並沒有多餘的符號
+		
+		"""
+		接下來要做的是
+		for index in range(len(col1)):
+			把col1[index].text.split() 裡面的第一個元素 Dec. or Oct ...etc ，及第二個元素 1,2,3,...,31 來找出當個月分最大的日期
+			if 日期如果是那月份最大的:
+				用個dictionary 把NAV，也就是把 col2[index].text.split() 放進那個那個 年/月 對應的NAV即可
+		"""
 
 #output.close()
