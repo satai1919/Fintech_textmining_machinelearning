a36b3357e040f4c264d57e2afa7431cbfe86331d
陳昱誠 <46038083+ga877439@users.noreply.github.com>
2019-03-12 22:02:23 +0800

    Update crawler.py

--- ETF_NAV/crawler.py
+++ ETF_NAV/crawler.py
@@ -36,15 +36,15 @@ for i in range(34):
 
         col1 = soup.find_all('td',attrs={"class": "col1"})  
         col2 = soup.find_all('td',attrs={"class": "col2"})  
-        print(col1[0].text.split())	#格式是這樣 ['Dec.', '31,', '2018']	一個list
-		print(col2[0].text.strip())	#格式是這樣 '123.54' 一個字串並沒有多餘的符號
-		
-		"""
-		接下來要做的是
-		for index in range(len(col1)):
-			把col1[index].text.split() 裡面的第一個元素 Dec. or Oct ...etc ，及第二個元素 1,2,3,...,31 來找出當個月分最大的日期
-			if 日期如果是那月份最大的:
-				用個dictionary 把NAV，也就是把 col2[index].text.split() 放進那個那個 年/月 對應的NAV即可
-		"""
+        print(col1[0].text.split())    #格式是這樣 ['Dec.', '31,', '2018']    一個list
+        print(col2[0].text.strip())    #格式是這樣 '123.54' 一個字串並沒有多餘的符號
+        
+        """
+        接下來要做的是
+        for index in range(len(col1)):
+            把col1[index].text.split() 裡面的第一個元素 Dec. or Oct ...etc ，及第二個元素 1,2,3,...,31 來找出當個月分最大的日期
+            if 日期如果是那月份最大的:
+                用個dictionary 把NAV，也就是把 col2[index].text.split() 放進那個那個 年/月 對應的NAV即可
+        """
 
 #output.close()
