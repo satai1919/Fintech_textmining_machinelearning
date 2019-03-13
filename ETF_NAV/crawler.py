15de12f9014cd642b06980b0a504140bd37c4a62
dd768092 <d768092@gmail.com>
2019-03-13 19:11:16 +0800

    finish output file

--- ETF_NAV/crawler.py
+++ ETF_NAV/crawler.py
@@ -6,21 +6,20 @@ import re
 import csv
 from bs4 import BeautifulSoup
 
-filename = "ETF_List_Filtered.csv"
-#filename = "..\data\ETF_List_Filtered.csv"
-file = open(filename, "r")
+filename = "ETF List Filtered.csv"
+inputfile = open(filename, "r")
 name = "output.csv"
 output = open(name, "w+", newline='')
 writer=csv.writer(output)
+#write first line(2018/12~2015/12)
 time_list=['']
 for i in range(2018,2015,-1):
     for j in range(12,0,-1):
         time_list.append(str(i)+'/'+str(j))
-print(time_list)
+time_list.append('2015/12')
 writer.writerow(time_list)
-#
-#
-a = file.readline()
+
+a = inputfile.readline()
 #a = a.split(",")
 ##print(a[-1])
 #output.write(""+a[0]+", "+a[-1]+"")
@@ -28,16 +27,16 @@ a = file.readline()
 
 # 34 etfs
 for i in range(34):
-    a = file.readline()
-    #print(a)
+    a = inputfile.readline()
     a = a.split(",")
-    #print(type(a))
-    # 15 pages in total
+
     session = login_website.login()
     df_list = []
     nav_dict=dict()
-    for j in range(1,16):
-        url = "https://ycharts.com/companies/"+a[0]+"/net_asset_value.json?endDate=12/31/2018&pageNum="+str(j)+"&startDate=12/31/2015"
+    #{'2018 Dec.':[31,123.54]} (key是字串，value是list[day,nat])
+
+    for j in range(1,17): # 16 pages in total
+        url = "https://ycharts.com/companies/"+a[0]+"/net_asset_value.json?endDate=12/31/2018&pageNum="+str(j)+"&startDate=12/1/2015"
         reqs = session.get(url)
         rj = json.loads(reqs.text)
         to_deal = rj["data_table_html"]
@@ -52,20 +51,19 @@ for i in range(34):
         date = [ col1[index].text.split() for index in range(len(col1)) ]
         nav = [ col2[index].text.strip() for index in range(len(col2)) ]
 
-        #dict('2018 Dec.')=[31,123.54]
-
         for index in range(len(col1)):
-            month=date[index][2]+" "+date[index][0]
+            month=date[index][2]+' '+date[index][0]
             day=int(re.sub(',', ' ', date[index][1]))
             if nav_dict.get(month)==None:
                 nav_dict[month]=[day,nav[index]]
             elif day>nav_dict[month][0]:
                 nav_dict[month]=[day,nav[index]]
 
-    print(list(nav_dict.items()))
     nav_list=[a[0]]
-    for item in list(nav_dict.items()):
+    for item in list(nav_dict.values()):
         nav_list.append(item[1])
     writer.writerow(nav_list)
+    print(a[0],'done')
 
-#output.close()
+inputfile.close()
+output.close()
