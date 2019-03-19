ea1ebd5a965f635755a7ab298d4dbaa46715acf7
dd768092 <d768092@gmail.com>
2019-03-20 01:50:53 +0800

    delete NaN

--- HW1/census/catchdata.py
+++ HW1/census/catchdata.py
@@ -4,6 +4,7 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.by import By
 import time
 import pandas as pd
+import datetime
 url="https://www.census.gov/econ/currentdata" #all the history data can be got from here
 driver=webdriver.Chrome()
 wait=WebDriverWait(driver,10)
@@ -23,5 +24,20 @@ s3.select_by_visible_text("New Orders")
 time.sleep(1)
 driver.find_element(By.XPATH,"//input[@type='submit']").click()
 url=driver.current_url
-table=pd.read_html(url)
-print(table)
+value=pd.read_html(url)[0]
+iniy=value.iloc[0,0]
+print(iniy)
+value=value.drop(labels='Year',axis=1)
+value=value.values
+value=value.flatten("C")
+value=pd.DataFrame(value).rename(columns={0:"Value"})
+
+d = []
+for i in range(len(value)):
+    a = datetime.date(iniy+i//12, 1+i%12, 1)
+    d.append(a)
+d = pd.DataFrame(d).rename(columns={0:"Date"})
+
+table = pd.DataFrame(columns=['Date'], data=d)
+table["Value"] = value
+print(table.dropna(axis=0,how='any').head(20))
