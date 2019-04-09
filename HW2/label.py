b0bf954e9389eab1e3bf0217ebc46f6a9b1f5fc6
許育銘 <d768092@nlp.csie.org>
2019-04-10 06:39:55 +0800

    label

---
+++ HW2/label.py
@@ -0,0 +1,43 @@
+import pandas as pd
+wordtype=dict()
+path = 'wordtype.csv'
+table = pd.read_csv(path).fillna('')
+for i in range(table.shape[0]):
+    if table.loc[i,'種類']!='':
+        wordtype[table.loc[i,'單詞']]=table.loc[i,'種類']
+
+filename = 'funds_info.csv'
+f = pd.read_csv(filename)
+
+content = f["投資策略"].tolist()
+
+trainfilename = 'train_data'
+trainfile = open(trainfilename, 'w')
+
+for article in content[:1500]:
+    article=str(article)
+    l = len(article)
+    now = 0
+    while now<l:
+        labeled=False
+        for key in wordtype.keys():
+            lkey = len(key)
+            if now+lkey<=l:
+                if article[now:now+lkey]==key:
+                    labeled=True
+                    trainfile.write(article[now]+'\t'+'B-'+wordtype[key]+'\n')
+                    for i in range(1,lkey):
+                        trainfile.write(article[now+i]+'\t'+'I-'+wordtype[key]+'\n')
+                    now += lkey
+                    break
+        if not labeled:
+            trainfile.write(article[now]+'\t'+'O'+'\n')
+            now += 1
+trainfile.close()
+
+testfilename = 'test_data'
+testfile = open(testfilename, 'w')
+for article in content[1500:]:
+    testfile.write(article)
+    testfile.write('\n')
+testfile.close()         
---
+++ HW2/set_type.py
@@ -0,0 +1,60 @@
+import pandas as pd
+import csv
+wordtype=dict()
+words=[]
+try:
+    path = 'wordtype.csv'
+    table = pd.read_csv(path).fillna('')
+    words = table['單詞'].tolist()
+    for word in words:
+        wordtype[word]=table.loc(word,'種類')
+except:
+    path = 'tf_idf2.csv'
+    words = pd.read_csv(path)['單詞'].tolist()
+    for word in words:
+        wordtype[word]=''
+
+types = {'LOC','IND','STY'} #set for valid type
+'''
+LOC: location
+IND: industry
+STY: style
+'''
+start=input('start word (default first word):')
+while True:
+    if start=='':
+        start=words[0]
+        break
+    elif start in words:
+        break
+    else:
+        input(start+' not found. Please type again (or just press enter and start at first word).') 
+
+start_type=False
+finish=False
+for word in words:
+    if not start_type:
+        if word == start:
+            start_type=True
+        else:
+            continue
+    if finish:
+        break
+    t=input('type of '+word+':')
+    while True:
+        if t in types:
+            wordtype[word]=t
+            break
+        elif t=='stop':
+            finish=True
+            break
+        elif t=='':
+            break
+        else:
+            print('error: please type one of the following:')
+            t=input('"LOC","IND","STY",or just press enter:')
+with open('wordtype.csv','w') as output:
+    writer=csv.writer(output)
+    writer.writerow(['單詞','種類'])
+    for word in words:
+        writer.writerow([word,wordtype[word]])
