006b2bad192ec988d0bf4ec64f03147c6e567ed8
satai1919 <love9420101@gmail.com>
2019-04-09 21:19:30 +0800

    Create correlation.py
    without any contents

---
+++ final project/correlation.py
@@ -0,0 +1,12 @@
+def correlation(a, b, period=100):
+	pass
+
+
+def person_corr(fundlist, period=100):
+	pass
+
+def rank_corr(fundlist, period=100):
+	pass
+
+def downside_corr(fundlist, period=100):
+	pass
