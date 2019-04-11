c7475138907775b9b0740073faaf3ef451b05b60
許育銘 <d768092@nlp.csie.org>
2019-04-11 10:05:25 +0800

    add end line

--- HW2/label.py
+++ HW2/label.py
@@ -5,6 +5,7 @@ table = pd.read_csv(path).fillna('')
 for i in range(table.shape[0]):
     if table.loc[i,'種類']!='':
         wordtype[table.loc[i,'單詞']]=table.loc[i,'種類']
+lastword={'。','？','！'}
 
 filename = 'funds_info.csv'
 f = pd.read_csv(filename)
@@ -19,30 +20,48 @@ testfile = open(testfilename, 'w')
 for i in range(len(content)):
     article=str(content[i])
     l = len(article)
-    now = 0
-    while now<l:
-        labeled=False
-        for key in wordtype.keys():
-            lkey = len(key)
-            if now+lkey<=l:
-                if article[now:now+lkey]==key:
-                    labeled=True
-                    if i%3==0:
+    if i%3==0:
+        now = 0
+        while now<l:
+            labeled=False
+            for key in wordtype.keys():
+                lkey = len(key)
+                if now+lkey<=l:
+                    if article[now:now+lkey]==key:
+                        labeled=True
                         testfile.write(article[now]+'\t'+'B-'+wordtype[key]+'\n')
                         for i in range(1,lkey):
                             testfile.write(article[now+i]+'\t'+'I-'+wordtype[key]+'\n')
-                    else:
+                        now += lkey
+                        break
+            if not labeled:
+                testfile.write(article[now]+'\t'+'O'+'\n')
+                if article[now] in lastword:
+                    testfile.write('\n')
+            now += 1
+        if article[-1] not in lastword:
+            testfile.write('\n')
+    else: 
+        now = 0
+        while now<l:
+            labeled=False
+            for key in wordtype.keys():
+                lkey = len(key)
+                if now+lkey<=l:
+                    if article[now:now+lkey]==key:
+                        labeled=True
                         trainfile.write(article[now]+'\t'+'B-'+wordtype[key]+'\n')
                         for i in range(1,lkey):
                             trainfile.write(article[now+i]+'\t'+'I-'+wordtype[key]+'\n')
-                    now += lkey
-                    break
-        if not labeled:
-            if i%3==0:
-                testfile.write(article[now]+'\t'+'O'+'\n')
-            else:
+                        now += lkey
+                        break
+            if not labeled:
                 trainfile.write(article[now]+'\t'+'O'+'\n')
+                if article[now] in lastword:
+                    trainfile.write('\n')
             now += 1
+        if article[-1] not in lastword:
+            trainfile.write('\n')
 trainfile.close()
 testfile.close()
 
