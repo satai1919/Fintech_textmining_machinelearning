a87079d7367efec4b95bee76fc2991f80636e7a3
許育銘 <d768092@nlp.csie.org>
2019-04-11 08:33:45 +0800

    change train and test

--- HW2/label.py
+++ HW2/label.py
@@ -13,33 +13,11 @@ content = f["投資策略"].tolist()
 
 trainfilename = 'train_data'
 trainfile = open(trainfilename, 'w')
-
-for article in content[:1500]:
-    article=str(article)
-    l = len(article)
-    now = 0
-    while now<l:
-        labeled=False
-        for key in wordtype.keys():
-            lkey = len(key)
-            if now+lkey<=l:
-                if article[now:now+lkey]==key:
-                    labeled=True
-                    trainfile.write(article[now]+'\t'+'B-'+wordtype[key]+'\n')
-                    for i in range(1,lkey):
-                        trainfile.write(article[now+i]+'\t'+'I-'+wordtype[key]+'\n')
-                    now += lkey
-                    break
-        if not labeled:
-            trainfile.write(article[now]+'\t'+'O'+'\n')
-            now += 1
-trainfile.close()
-
 testfilename = 'test_data'
 testfile = open(testfilename, 'w')
 
-for article in content[1500:]:
-    article=str(article)
+for i in range(len(content)):
+    article=str(content[i])
     l = len(article)
     now = 0
     while now<l:
@@ -49,12 +27,22 @@ for article in content[1500:]:
             if now+lkey<=l:
                 if article[now:now+lkey]==key:
                     labeled=True
-                    testfile.write(article[now]+'\t'+'B-'+wordtype[key]+'\n')
-                    for i in range(1,lkey):
-                        testfile.write(article[now+i]+'\t'+'I-'+wordtype[key]+'\n')
+                    if i%3==0:
+                        testfile.write(article[now]+'\t'+'B-'+wordtype[key]+'\n')
+                        for i in range(1,lkey):
+                            testfile.write(article[now+i]+'\t'+'I-'+wordtype[key]+'\n')
+                    else:
+                        trainfile.write(article[now]+'\t'+'B-'+wordtype[key]+'\n')
+                        for i in range(1,lkey):
+                            trainfile.write(article[now+i]+'\t'+'I-'+wordtype[key]+'\n')
                     now += lkey
                     break
         if not labeled:
-            testfile.write(article[now]+'\t'+'O'+'\n')
+            if i%3==0:
+                testfile.write(article[now]+'\t'+'O'+'\n')
+            else:
+                trainfile.write(article[now]+'\t'+'O'+'\n')
             now += 1
-testfile.close()         
+trainfile.close()
+testfile.close()
+
