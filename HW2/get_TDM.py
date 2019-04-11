cb034784e64b2757e9a6fb9bcd14c3a9c7fe437a
許育銘 <d768092@nlp.csie.org>
2019-04-12 00:48:38 +0800

    Co-occurence

--- HW2/get_TDM.py
+++ HW2/get_TDM.py
@@ -1,11 +1,13 @@
 import ast
 import pandas as pd
+import numpy as np
 sentence_count = 0 
 
 word_frag = False
 word = ''
 df_list = []
-with open( r'C:\Users\User\Documents\GitHub\HW1\HW2\from NER\data_path_save\1554975189\results\label_test', 'r') as file:
+#with open( r'from NER\data_path_save\1554975189\results\label_test', 'r') as file:
+with open( r'from NER/data_path_save/1554975189/results/label_test', 'r') as file:
 	tem_dict = dict()
 	for line in file:
 		
@@ -29,7 +31,7 @@ with open( r'C:\Users\User\Documents\GitHub\HW1\HW2\from NER\data_path_save\1554
 		
 		if tokens[1] == '0':
 			if not word_frag:
-				print(word)
+				#print(word)
 				try:
 					tem_dict[word][0] +=1
 				except:
@@ -44,7 +46,24 @@ with open( r'C:\Users\User\Documents\GitHub\HW1\HW2\from NER\data_path_save\1554
 		elif tokens[1][0] == 'I':	
 			word += s
 			
-print(df_list[0:5])		
+#print(df_list[0:5])		
 		
-df = pd.concat(df_list)
+df = pd.concat(df_list).fillna(0)
+'''
+f = pd.read_csv('funds_info.csv')
+fundname=f['基金id'].tolist()
+fundname=[ str(fundname[i]) for i in range(len(fundname)) if i%3==0 ]
+for i in range(len(fundname)):
+    df.rename(index={i:fundname[i]}, inplace=True)
+'''
+names=[ '基金'+str(i) for i in range(df.shape[0])]
+df.set_axis(names, axis='index', inplace=True)
 print(df)
+df.to_csv('TDM.csv')
+cov=df.values
+cov=cov.dot(np.transpose(cov))
+df2=pd.DataFrame(cov)
+for i in range(df2.shape[0]):
+    df2.rename(columns={i:'基金'+str(i)}, index={i:'基金'+str(i)}, inplace=True)
+print(df2)
+df2.to_csv('Co-occur.csv')
