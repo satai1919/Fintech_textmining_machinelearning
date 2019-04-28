39a0a286a5fad9b5e4592441f4362572ae737f45
satai1919 <love9420101@gmail.com>
2019-04-28 21:41:15 +0800

    Update correlation.py

--- final project/correlation.py
+++ final project/correlation.py
@@ -1,12 +1,25 @@
-def correlation(a, b, period=100):
-	pass
+#假設是dataframe的狀態
+import nunpy as np
+from scipy.stats.stats import pearsonr, spearmanr
 
+def person_corr(fundlist, a, b, period=100):
+    funda = fundlist[""+a+""].tail(period).values
+    fundb = fundlist[""+b+""].tail(period).values
+    corr, _= pearsonr(funda, fundb)
+    return corr
+    
 
-def person_corr(fundlist, period=100):
-	pass
+def rank_corr(fundlist, a, b, period=100):
+    funda = fundlist[""+a+""].tail(period).values
+    fundb = fundlist[""+b+""].tail(period).values
+    corr, _= spearmanr(funda, fundb)
+    return corr
 
-def rank_corr(fundlist, period=100):
-	pass
-
-def downside_corr(fundlist, period=100):
-	pass
+def downside_corr(fundlist, a, b, period=100):
+    funda = fundlist[""+a+""].tail(period).values
+    fundb = fundlist[""+b+""].tail(period).values
+    i, = np.where(funda<0)
+    corrab, _ = pearsonr(funda[i], fundb[i])
+    j, = np.where(fundb<0)
+    corrba, _ = pearsonr(funda[j], fundb[j])
+    return corrab, corrba
