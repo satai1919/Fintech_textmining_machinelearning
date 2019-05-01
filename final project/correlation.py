b3c3e54ff32bbf26d6b5e49c5f4aebb33826aee2
dd768092 <d768092@gmail.com>
2019-05-01 20:22:10 +0800

    dropna and correct downside

--- final project/correlation.py
+++ final project/correlation.py
@@ -1,25 +1,29 @@
 #假設是dataframe的狀態
-import nunpy as np
+import numpy as np
 from scipy.stats.stats import pearsonr, spearmanr
 
 def person_corr(fundlist, a, b, period=100):
-    funda = fundlist[""+a+""].tail(period).values
-    fundb = fundlist[""+b+""].tail(period).values
+    fundlist = fundlist[[a,b]].tail(period).dropna()
+    funda = fundlist[a].values
+    fundb = fundlist[b].values
     corr, _= pearsonr(funda, fundb)
-    return corr
+    return corr, fundlist.shape[0]
     
-
 def rank_corr(fundlist, a, b, period=100):
-    funda = fundlist[""+a+""].tail(period).values
-    fundb = fundlist[""+b+""].tail(period).values
+    fundlist = fundlist[[a,b]].tail(period).dropna()
+    funda = fundlist[a].values
+    fundb = fundlist[b].values
     corr, _= spearmanr(funda, fundb)
     return corr
 
 def downside_corr(fundlist, a, b, period=100):
-    funda = fundlist[""+a+""].tail(period).values
-    fundb = fundlist[""+b+""].tail(period).values
-    i, = np.where(funda<0)
+    fundlist = fundlist[[a,b]].tail(period).dropna()
+    funda = fundlist[a].values
+    funda_last = np.concatenate((np.zeros(1,),funda[:-1]))
+    fundb = fundlist[b].values
+    fundb_last = np.concatenate((np.zeros(1,),fundb[:-1]))
+    i, = np.where(funda<funda_last)
     corrab, _ = pearsonr(funda[i], fundb[i])
-    j, = np.where(fundb<0)
+    j, = np.where(fundb<fundb_last)
     corrba, _ = pearsonr(funda[j], fundb[j])
     return corrab, corrba
