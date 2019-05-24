cc55269bcb752bd0cb87cdf10513bb6333a2faf7
dd768092 <d768092@gmail.com>
2019-05-24 21:38:12 +0800

    add correlation show

--- final project/correlation.py
+++ final project/correlation.py
@@ -3,13 +3,15 @@ import numpy as np
 from scipy.stats.stats import pearsonr, spearmanr
 
 def person_corr(fundlist, a, b, period=100):
+    period = min(period,fundlist.shape[0])
     fundlist = fundlist[[a,b]].tail(period).dropna()
     funda = fundlist[a].values
     fundb = fundlist[b].values
     corr, _= pearsonr(funda, fundb)
-    return corr, fundlist.shape[0]
+    return corr
     
 def rank_corr(fundlist, a, b, period=100):
+    period = min(period,fundlist.shape[0])
     fundlist = fundlist[[a,b]].tail(period).dropna()
     funda = fundlist[a].values
     fundb = fundlist[b].values
@@ -17,13 +19,13 @@ def rank_corr(fundlist, a, b, period=100):
     return corr
 
 def downside_corr(fundlist, a, b, period=100):
+    period = min(period,fundlist.shape[0])
     fundlist = fundlist[[a,b]].tail(period).dropna()
     funda = fundlist[a].values
-    funda_last = np.concatenate((np.zeros(1,),funda[:-1]))
     fundb = fundlist[b].values
     fundb_last = np.concatenate((np.zeros(1,),fundb[:-1]))
-    i, = np.where(funda<funda_last)
-    corrab, _ = pearsonr(funda[i], fundb[i])
-    j, = np.where(fundb<fundb_last)
-    corrba, _ = pearsonr(funda[j], fundb[j])
-    return corrab, corrba
+    i = funda[1:]<funda[:-1]
+    corrab, _ = pearsonr(funda[1:][i], fundb[1:][i])
+    j = fundb[1:]<fundb[:-1]
+    corrba, _ = pearsonr(funda[1:][j], fundb[1:][j])
+    return (corrab+corrba)/2
